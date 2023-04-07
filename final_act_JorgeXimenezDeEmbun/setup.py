86253087092c0eaebc103b63c67431d9184c3d59
Jorge Ximenez <jorge_x93@hotmail.com>
2023-04-07 11:33:33 +0200

    FINAL ACT WORKING WITH PIPENV & DATABASE

--- final_act_JorgeXimenezDeEmbun/setup.py
+++ final_act_JorgeXimenezDeEmbun/setup.py
@@ -4,7 +4,7 @@ setup(
     name="my_app",
     version="0.1",
     packages=["my_app"],
-    install_requires=["requests", "matplotlib", "sqlite3"],
+    install_requires=["requests", "matplotlib"],
     entry_points={
         "console_scripts": [
             "my_app = my_app.__main__:main"
